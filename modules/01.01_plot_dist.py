d1610db7bef73fa7148997457806c66a56a03c23
wyu <kharma@gmail.com>
2021-04-07 21:22:41 -0700

    refactor histogram

--- modules/01.01_plot_dist.py
+++ modules/01.01_plot_dist.py
@@ -2,6 +2,10 @@
 
 # plot distribution
 # https://www.datacamp.com/community/tutorials/python-data-profiling
-sns.distplot(
-    df["SegScore"].dropna()
+sns.histplot(
+    df["SegScore"].dropna(),
+    bins=10
 ).set_title('Distribution of Segment Score Rating')
+# visualize histogram with log scale
+# https://stackoverflow.com/questions/23913151/log-log-lmplot-with-seaborn
+plt.yscale('log')
