cf03bacc158c91448a5be9ed8085dfacda2fa9b1
wyu <kharma@gmail.com>
2021-04-07 21:22:29 -0700

    move train/test split

--- modules/01.00_setup.py
+++ modules/01.00_setup.py
@@ -34,3 +34,48 @@ print('Output dataset info:')
 print(df.info(), '\n')
 print('Output unique values for segment name:')
 print(df['FullName'].unique(), '\n')
+
+# filter data for model train/predict
+# https://pandas.pydata.org/docs/getting_started/intro_tutorials/03_subset_data.html
+df_subset = df[[
+    'OBJECTID',
+    'SegmentID',
+    'Seg_ID_Sco',
+    'Seg_Bk_Sco',
+    'Seg_LL_Sco',
+    'Seg_Wd_Sco',
+    'SegScore'
+]]
+
+# split train and test data
+# https://stackoverflow.com/questions/24147278/how-do-i-create-test-and-train-samples-from-one-dataframe-with-pandas
+print('Output df shape:')
+print(df.shape, '\n')
+train, test = train_test_split(df_subset, test_size=0.2)
+print('Output train df shape:')
+print(train.shape, '\n')
+print('Output test df shape:')
+print(test.shape, '\n')
+
+# train rf classifier
+# https://stackabuse.com/classification-in-python-with-scikit-learn-and-pandas/
+y_train = train.iloc[:,6]
+X_train = train.iloc[:,:6]
+y_test = test.iloc[:,6]
+X_test = test.iloc[:,:6]
+
+# encode labels as continuous
+# https://stackoverflow.com/questions/41925157/logisticregression-unknown-label-type-continuous-using-sklearn-in-python
+# lab_enc = preprocessing.LabelEncoder()
+# y_train = lab_enc.fit_transform(y_train)
+# y_test = lab_enc.fit_transform(y_test)
+y_train = y_train.astype('int64')
+y_test = y_test.astype('int64')
+print('Output X_train df info:')
+print(X_train.info(), '\n')
+print('Output X_test df info:')
+print(X_test.info(), '\n')
+print('Output y_train df info:')
+print(y_train.dtype, '\n')
+print('Output y_test df info:')
+print(y_test.dtype, '\n')
