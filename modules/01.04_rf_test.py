01a05c2666dc90a7e51db7de4e2a1684ec7b3190
wyu <kharma@gmail.com>
2021-04-07 21:23:21 -0700

    add grid search

--- modules/01.04_rf_test.py
+++ modules/01.04_rf_test.py
@@ -2,9 +2,12 @@
 
 # make predictions and calculate accuracy score
 # https://www.blopig.com/blog/2017/07/using-random-forests-in-python-with-scikit-learn/
-predicted = RF.predict(X_test)
+# rfc_params.predict(X_test)
+# print('Random Classifier Accuracy Score:')
+# round(rfc_params.score(X_test,y_test), 4)
+predicted = rfc_params.predict(X_test)
 accuracy = accuracy_score(y_test, predicted)
-print(f'Out-of-bag score estimate: {RF.oob_score_:.4}')
+print(f'Out-of-bag score estimate: {rfc_params.oob_score_:.4}')
 print(f'Mean accuracy score: {accuracy:.4}')
 
 # create/plot confusion matrix
